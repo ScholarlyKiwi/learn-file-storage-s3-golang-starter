d822e7de08d62a802fcc24e45b62a14faed69338
ScholarlyKiwi <91203972+ScholarlyKiwi@users.noreply.github.com>
2026-03-18 13:52:02 +1300

    CH1: File Storage L5: Multipart Load

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,39 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusRequestHeaderFieldsTooLarge, "Error parsing form", err)
+		return
+	}
+
+	thumbnail_file, thumbnail_header, err := r.FormFile("thumbnail")
+	media_type := thumbnail_header.Header.Get("Content-Type")
+	thumbnail_bytes, err := io.ReadAll(thumbnail_file)
+
+	video_metadata, err := cfg.db.GetVideo(videoID)
+
+	if video_metadata.CreateVideoParams.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User is not the author.", fmt.Errorf("UserID does not match authorID"))
+	}
+
+	thumbnail := thumbnail{
+		data:      thumbnail_bytes,
+		mediaType: media_type,
+	}
+	videoThumbnails[videoID] = thumbnail
+
+	thumbnail_url := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v", cfg.port, videoIDString)
+	video_metadata.ThumbnailURL = &thumbnail_url
+
+	err = cfg.db.UpdateVideo(video_metadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error Updating Video", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, video_metadata)
 }
